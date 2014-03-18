7a57e271f558149e9c9b9d96adc8be2760669bef
Jay Zhai <jay.zhai@gmail.com>
2014-03-18 17:45:26 +0800

    write model to json

--- bpm.py
+++ bpm.py
@@ -1,16 +1,20 @@
 __author__ = 'Jay Zhai'
 
 import json
-from workflow import Node
+from workflow import Node, Workflow
 
 def convert_to_builtin_type(obj):
-    #print 'default(', repr(obj), ')' # 把MyObj对象转换成dict类型的对象
+    #print 'default(', repr(obj), ')'
     d = {}
     d.update(obj.__dict__)
     return d
 
 def from_node_to_json(node):
     return json.dumps(node, default=convert_to_builtin_type)
+
+def from_workflow_to_json(workflow):
+    return json.dumps(workflow, default=convert_to_builtin_type)
+
 
 
 def from_json_to_node(_str):
--- workflow.py
+++ workflow.py
@@ -1,14 +1,29 @@
 __author__ = 'Jay Zhai'
 
 
-class WorkflowDef:
+class Workflow:
     'A workflow definition'
 
-    def __init__(self, name, version=0):
+    def __init__(self, name, instanceid='0', version=0):
+        self.instanceid = instanceid
         self.name = name
         self.version = version
+        self.root_node = Node('_ROOTNODE')
+        self.node_dict = {'_ROOTNDE': self.root_node}
+        self.pointer = '_ROOTNODE'
 
+    def find_node(self, name):
+        return self.node_dict[name]
 
+    def add_node(self, node):
+        self.node_dict[node.name] = node
+
+    def delete_node(self, node_name):
+        return self.node_dict.pop(node_name)
+
+    def flow_to(self, node_name):
+        #TODO
+        pass
 
 
 
@@ -17,3 +32,8 @@ class Node:
 
     def __init__(self, name):
         self.name = name
+        self.transit_dict = {}
+
+
+    def transit(self,  target_node, transit_tag='DEFAULT'):
+        self.transit_dict[transit_tag] = target_node
